aa02fe66594fde3391c7467b8a6f6d58c5a7cea4
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-10-15 13:55:37 -0700

    post route and timestamp

---
+++ dbschema.js
@@ -0,0 +1,13 @@
+let db  = {
+
+    screams:    [
+        {
+            userHandle:     'user',
+            body:           'this is the scream body',
+            createdAt:      '2019-10-15T01:45:25.018Z',
+            likeCount:      5,
+            commentCount:   2
+        }
+    ]
+
+}
--- functions/index.js
+++ functions/index.js
@@ -17,11 +17,16 @@ const app       = express()
 
 app.get('/screams', (req, res) => {
 
-    admin.firestore().collection('screams').get()
+    admin.firestore().collection('screams').orderBy('createdAt', 'desc').get()
         .then((data) => {
             let screams = []
             data.forEach((doc) =>{
-                screams.push(doc.data())
+                screams.push({
+                    screamId:   doc.id,
+                    body:       doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt:  doc.data().createdAt
+                })
             })
             return res.json(screams)
         })
@@ -30,7 +35,7 @@ app.get('/screams', (req, res) => {
 })
 
 
-app.post('/screams', (req, res) => {
+app.post('/scream', (req, res) => {
 
     if(req.method !== 'POST'){
         return res.status(400).json({ error: 'Method not allowed' })
@@ -39,7 +44,7 @@ app.post('/screams', (req, res) => {
     const newScream = {
         body:       req.body.body,
         userHandle: req.body.userHandle,
-        createdAt:  admin.firestore.Timestamp.fromDate(new Date())
+        createdAt:  new Date().toISOString()
     }
 
     admin.firestore()
