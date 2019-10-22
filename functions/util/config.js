549e09198c43ebff6ec4e0fdfd06c979bd705dfb
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-10-21 23:47:10 -0700

    refactoring and organizing

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+// Configuration Export
+
+module.exports = {
+    apiKey: "AIzaSyBjz1ARYXRXfuikgivVpyfjWbOCtzi_ZZw",
+    authDomain: "social-media-tutorial-backend.firebaseapp.com",
+    databaseURL: "https://social-media-tutorial-backend.firebaseio.com",
+    projectId: "social-media-tutorial-backend",
+    storageBucket: "social-media-tutorial-backend.appspot.com",
+    messagingSenderId: "177605819920"
+}
